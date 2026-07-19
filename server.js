188efcc2193d0c82db9f25616a9ca8c20a0a4b48
Ebenezer <your-ebenezeradeniran06@.com>
2026-07-19 18:20:16 +0100

    Added logging middleware

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require("express");
-
 require("dotenv").config();
 
 const app = express();
 
-
-const PORT = process.env.PORT;
-
+// Parse JSON data
 app.use(express.json());
 
-// Home Route
+// Middleware to log requests
+app.use((req, res, next) => {
+    console.log("Request:", req.method, req.url);
+    next();
+});
+
+// Home route
 app.get("/", (req, res) => {
     res.send("My Week 2 API!");
 });
 
-// POST Route
+// Create user
 app.post("/user", (req, res) => {
-
-    const { name, email } = req.body;
+    const name = req.body.name;
+    const email = req.body.email;
 
     if (!name || !email) {
         return res.status(400).json({
@@ -26,23 +29,22 @@ app.post("/user", (req, res) => {
     }
 
     res.json({
-        message: `Hello, ${name}!`
+        message: "Hello " + name + "!"
     });
-
 });
 
-// GET User by ID
+// Get user by ID
 app.get("/user/:id", (req, res) => {
-
     const id = req.params.id;
 
     res.json({
-        message: `User ${id} profile`
+        message: "User " + id + " profile"
     });
-
 });
 
-// Start Server
+// Start server
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log("Server is running on port " + PORT);
 });
